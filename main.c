f0e002edab5a8432840d922bb08927b6767fa48b
Hasan Gunes <99250362+hasangns@users.noreply.github.com>
2024-12-05 21:01:43 +0300

    Update main.c

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 #include "raymath.h"
 #include <stdbool.h>
 
-#define WIDTH 800
-#define HEIGTH 600
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGTH 600
 
-#define PLAYER_X 100
+#define PLAYER_X 50
 #define PLAYER_Y 500
 
 
@@ -16,94 +16,120 @@ typedef struct GameState
     Vector2 playerSize;
     float playerVelocity;
     bool playerOnGround;
-    
-    
+
+
+    Vector2 enemyPos;
+    Vector2 enemySize;
+
     float gravity;
-    float elapsedTime;
-    
+    float spaceTime;
+    int linePos;
+
 }GameState;
 
 
 int main(void)
 {
-    
-    InitWindow(WIDTH, HEIGTH, "Dinosaur Clone");
-    
+
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGTH, "Dinosaur Clone");
+
     GameState gameState = {
-        
+
         .playerPos = {PLAYER_X, PLAYER_Y},
         .playerSize = {40, 40},
         .gravity = 1000.0f,
-        .playerOnGround = true
+        .enemySize = {20, 60},
+        .linePos = 0
+        
     };
-    
-    
-    while(!WindowShouldClose())
+
+    Camera2D camera;
+    camera.target = gameState.playerPos;
+    camera.offset = (Vector2){ SCREEN_WIDTH / 2.0f, SCREEN_HEIGTH / 2.0f};
+    camera.rotation = 0.0f;
+    camera.zoom = 1.0f;
+
+
+    while (!WindowShouldClose())
     {
-        
-        
+
+
         // Update player
         {
-         
-            
+
             gameState.playerPos.y += gameState.playerVelocity * GetFrameTime();
             gameState.playerVelocity += gameState.gravity * GetFrameTime();
-            
+
             if (IsKeyDown(KEY_SPACE) && gameState.playerOnGround) {
-                gameState.playerVelocity = -1000.0f;
-                gameState.playerOnGround = false;
+                gameState.spaceTime += GetFrameTime();
             }
-            
-            if (gameState.playerPos.y < 350) {
-                gameState.playerVelocity = 50.0f;
+
+            if (IsKeyReleased(KEY_SPACE) && gameState.playerOnGround) {
+                if (gameState.spaceTime > 0.25f) {
+                    gameState.playerVelocity = -600.0f;
+                }
+                else {
+                    gameState.playerVelocity = -500.0f;
+                }
+                gameState.spaceTime = 0.0f;
             }
-            
-           
-            
-            if(gameState.playerPos.y > 500){
+
+            if (gameState.playerPos.y >= 500) {
                 gameState.playerPos.y = 500;
                 gameState.playerOnGround = true;
             }
-            
-            
-//            if (defaultState.playerVelocity == false) {
-//                defaultState.playerPos.y += GetFrameTime() * defaultState.gravity;
-//            }
-            
-//            else{
-//                defaultState.playerVelocity = false;
-//            }
+            else {
+                gameState.playerOnGround = false;
+            }
 
-                    
+
+
         }
-        
-        
+
+        {
+            camera.target = (Vector2){ gameState.playerPos.x + 350 ,300};
+
+        }
+
+
+
+        // Enemies
+        {
+            gameState.enemyPos.x = GetRandomValue(gameState.playerPos.x + 100, gameState.playerPos.x + SCREEN_WIDTH);
+
+
+
+        }
+
+
+        // Update Line
+        {
+
+            gameState.playerPos.x += 400 * GetFrameTime();
+
+        }
+
         BeginDrawing();
         ClearBackground(GRAY);
-        
+        BeginMode2D(camera);
+
         // Draw player
         {
             DrawRectangleV(gameState.playerPos, gameState.playerSize, RED);
-            DrawText(TextFormat("velocity : %f", gameState.playerVelocity), gameState.playerPos.x, gameState.playerPos.y - 20, 20, BLACK);
-            DrawText(TextFormat("velocity : %d", gameState.playerOnGround), gameState.playerPos.x, gameState.playerPos.y - 60, 20, BLACK);
-            DrawText(TextFormat("player y : %f", gameState.playerPos.y), gameState.playerPos.x, gameState.playerPos.y - 40, 20, BLACK);
+            DrawLine(gameState.playerPos.x - SCREEN_WIDTH, 540, gameState.playerPos.x + SCREEN_WIDTH, 540, RED);
+            DrawRectangle(1000, 400, 20, 60, BLUE);
 
+            //DrawText(TextFormat("velocity : %f", gameState.playerVelocity), gameState.playerPos.x, gameState.playerPos.y - 20, 20, BLACK);
+            //DrawText(TextFormat("player y : %f", gameState.playerPos.y), gameState.playerPos.x, gameState.playerPos.y - 40, 20, BLACK);
+            //DrawText(TextFormat("holding space : %f", gameState.spaceTime), gameState.playerPos.x, gameState.playerPos.y - 60, 20, BLACK);
+            //DrawText(TextFormat("on ground : %d", gameState.playerOnGround), gameState.playerPos.x, gameState.playerPos.y - 80, 20, BLACK);
         }
-        
-        
-        
+        EndMode2D();
         EndDrawing();
-        
+
     }
-    
     CloseWindow();
-    
-    
+
     return 0;
-    
-    
 
-    
-
-    
 }
